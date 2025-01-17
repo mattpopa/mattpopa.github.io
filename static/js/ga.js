ce4e0c9a73a51f993703718debad7ae317d086c4
Matei Popa <matei@siteassist.co>
2025-01-17 02:03:28 +0200

    add cookie consent

---
+++ static/js/ga.js
@@ -0,0 +1,7 @@
+// Google Analytics tracking code
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+// Replace 'G-6CK7S9C8HQ' with your actual tracking ID
+gtag('config', 'G-6CK7S9C8HQ');
